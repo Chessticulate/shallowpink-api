4b3bd372a7fecd54c234a8385a251b2914779f2c
Brian Glaws <glaws.brian@gmail.com>
2023-12-21 17:12:21 -0500

    added crud tests

--- tests/test_crud.py
+++ tests/test_crud.py
@@ -3,6 +3,7 @@ from app.config import CONFIG
 from app.models import ResponseType, GameType
 import jwt
 import pytest
+import sqlalchemy
 
 def test_password_hashing():
     pswd = "test password"
@@ -14,6 +15,8 @@ def test_password_hashing():
 def test_get_user_by_name(init_fake_user_data, fake_user_data):
     # test get user that doesnt exist
 
+    assert crud.get_user_by_name("baduser") is None
+
     for data in fake_user_data:
         user = crud.get_user_by_name(data["name"])
         assert user.email == data["email"]
@@ -21,6 +24,9 @@ def test_get_user_by_name(init_fake_user_data, fake_user_data):
 # new
 def test_get_user_by_id(init_fake_user_data, fake_user_data):
     # test bad id, not an integer
+
+    assert crud.get_user_by_id("apple") is None
+    
     id_ = 1
     for data in fake_user_data:
         user = crud.get_user_by_id(id_)
@@ -28,8 +34,6 @@ def test_get_user_by_id(init_fake_user_data, fake_user_data):
         assert user.name == data["name"]
 
 def test_create_user(drop_all_users):
-    # test creating users with duplicate id's emails usernames etc
-    # username too long/short, invalid chars
     fake_name = "cleo"
     fake_email = "cleo@dogmail.com"
     fake_password = "IloveKongs"
@@ -41,7 +45,19 @@ def test_create_user(drop_all_users):
     assert user.name == fake_name
     assert user.email == fake_email
     assert crud.check_password(fake_password, user.password)
+
+    # test creating users with duplicate id's emails usernames etc 
+    # username too long/short, invalid chars
     
+    fake_name2 = "ally" 
+    fake_email2 = "cleo@dogmail.com"
+    fake_pass2 = "treats"
+    
+    with pytest.raises(sqlalchemy.exc.IntegrityError):
+        crud.create_user(fake_name2, fake_email2, fake_pass2)
+    with pytest.raises(sqlalchemy.exc.IntegrityError):
+        crud.create_user(fake_name, fake_email, fake_password)
+ 
 def test_login_validate_token(init_fake_user_data):
     # test expired token
 
@@ -56,7 +72,9 @@ def test_login_validate_token(init_fake_user_data):
 
 # new
 def test_create_invite(init_fake_user_data):
-    # test creating invite with non exitent users
+
+    # both users must exist for a valid invite
+    assert crud.create_invite("fakeuser1", "baduser") is None
     
     invite = crud.create_invite("fakeuser1", "fakeuser2")
 
@@ -67,7 +85,10 @@ def test_create_invite(init_fake_user_data):
     
 # new 
 def test_get_invite(init_fake_user_data):
-    # test getting invite with non existent users
+    # invitation cannot be made with non existent users
+
+    # get_invite accepts int id
+    assert crud.get_invite("id") is None
 
     invitation = crud.create_invite("fakeuser1", "fakeuser2")    
     invite = crud.get_invite(invitation.id)
