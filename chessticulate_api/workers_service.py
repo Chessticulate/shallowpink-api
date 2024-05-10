68918bd515f8e0ec0e89ae7e22fb38b827fa1621
Brian Glaws <glaws.brian@gmail.com>
2024-05-10 14:32:14 -0400

    added workers service tests

--- chessticulate_api/workers_service.py
+++ chessticulate_api/workers_service.py
@@ -11,16 +11,35 @@ import httpx
 from chessticulate_api.config import CONFIG
 
 
+class ServerRequestError(Exception):
+    """ Server Request Error Exception class """
+
+
+class ClientRequestError(Exception):
+    """ CLient Request Error Exception class """
+
+
 async def do_move(fen: str, move: str, states: dict[str, str]):
     """do move request to chess-workers service"""
     client = httpx.AsyncClient()
     try:
         response = await client.post(
-            CONFIG.workers_url, data={"fen": fen, "move": move, "states": states}
+            CONFIG.workers_url, json={"fen": fen, "move": move, "states": states}
         )
-        if response.status != 200:
-            return None
-        return response.json()
+        if response.status_code == 200:
+            return response.json()
+
+        if 400 <= response.status_code < 500:
+            if response.json()["message"] in [
+                "invalid move",
+                "move puts player in check",
+                "player is still in check",
+                "the game is already over",
+            ]:
+                raise ClientRequestError(response.json())
+
+        raise ServerRequestError(response.json())
+
     finally:
         await client.aclose()
 
@@ -30,10 +49,18 @@ async def suggest_move(fen: str, states: dict[str, str]):
     client = httpx.AsyncClient()
     try:
         response = await client.post(
-            CONFIG.workers_url, data={"fen": fen, "states": states}
+            CONFIG.workers_url, json={"fen": fen, "states": states}
         )
-        if response.status != 200:
-            return None
-        return response.json()
+        if response.status_code == 200:
+            return response.json()
+
+        if 400 <= response.status_code < 500:
+            if response.json()["message"] in [
+                "the game is already over",
+            ]:
+                raise ClientRequestError(response.json())
+
+        raise ServerRequestError(response.json())
+
     finally:
         await client.aclose()
