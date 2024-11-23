78f97e30b8b001a2a38f7db83160b7dc4e48025b
Kyle Glaws <kyle.glaws@gmail.com>
2024-11-23 18:43:24 -0500

    add timestamp to uvicorn logs

--- chessticulate_api/__main__.py
+++ chessticulate_api/__main__.py
@@ -1,12 +1,16 @@
 """Chessticulate API Entrypoint"""
 
 import uvicorn
+from uvicorn.config import LOGGING_CONFIG
 
 from chessticulate_api.config import CONFIG
 
 
 def main():
     """run API with uvicorn"""
+
+    LOGGING_CONFIG["formatters"]["default"]["fmt"] = "%(asctime)s %(levelprefix)s %(message)s"
+
     uvicorn.run(
         "chessticulate_api:app",
         host=CONFIG.app_host,
